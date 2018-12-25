8530425eb55c507e3e9b2344b3b36a40d4ca193a
liuzy <liuziyingbeidou@163.com>
2018-12-25 17:49:15 +0800

    init xtomcat
    init xtomcat
    
    Signed-off-by: liuzy <liuziyingbeidou@163.com>

---
+++ xtomcat-v1/src/main/java/com/venux/XtomcatServer.java
@@ -0,0 +1,72 @@
+package com.venux; /**
+ * Created by Administrator on 2018/12/25.
+ */
+
+import com.venux.http.Request;
+import com.venux.http.Response;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 静态文件
+ *
+ * @author Alan Liu
+ * @create 2018-12-25 16:05
+ **/
+public class XtomcatServer {
+
+    public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webroot";
+
+    private static final String SHUTDOWN_COMMAND = "/SHUTDOWN";
+
+    private boolean shutdown = false;
+
+    public static void main(String[] args){
+        XtomcatServer server = new XtomcatServer();
+        server.await();
+    }
+
+    public void await(){
+        ServerSocket serverSocket = null;
+        int port = 80;
+        try {
+            serverSocket = new ServerSocket(port,1, InetAddress.getByName("127.0.0.1"));
+        }catch (IOException e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        while (!shutdown){
+            Socket socket = null;
+            InputStream input = null;
+            OutputStream output = null;
+            try {
+                socket = serverSocket.accept();
+                input = socket.getInputStream();
+                output = socket.getOutputStream();
+
+                Request request = new Request(input);
+                request.parse();
+
+                Response response = new Response(output);
+                response.setRequest(request);
+                response.sendStaticResource();
+
+                socket.close();
+
+                shutdown = request.getUri().equals(SHUTDOWN_COMMAND);
+
+            }catch (Exception e){
+                e.printStackTrace();
+                continue;
+            }
+        }
+    }
+
+}
---
+++ xtomcat-v1/src/main/java/com/venux/http/Request.java
@@ -0,0 +1,55 @@
+package com.venux.http;/**
+ * Created by Administrator on 2018/12/25.
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Request
+ *
+ * @author Alan Liu
+ * @create 2018-12-25 15:26
+ **/
+public class Request {
+
+    private InputStream input;
+    private String uri;
+
+    public Request(InputStream input){
+        this.input = input;
+    }
+
+    public void parse(){
+        StringBuffer request = new StringBuffer(2048);
+        int i;
+        byte[] buffer = new byte[2048];
+        try{
+            i = input.read(buffer);
+        }catch (IOException e){
+            e.printStackTrace();
+            i = -1;
+        }
+        for (int j = 0; j < i; j++){
+            request.append((char) buffer[j]);
+        }
+        System.out.println(request.toString());
+        uri = parseUri(request.toString());
+    }
+
+    public String parseUri(String requestString){
+        int index1,index2;
+        index1 = requestString.indexOf(' ');
+        if (index1 != -1){
+            index2 = requestString.indexOf(' ', index1 + 1);
+            if (index2 > index1){
+                return requestString.substring(index1 + 1, index2);
+            }
+        }
+        return null;
+    }
+
+    public String getUri(){
+        return uri;
+    }
+}
